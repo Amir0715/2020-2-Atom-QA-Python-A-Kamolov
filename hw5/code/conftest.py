b6268ae5b6a14a317b72af18bd885cfc12e5e427
Amir <kamolov.amir2000@yandex.ru>
2020-11-02 23:10:37 +0300

    Написал тесты.

--- hw5/code/conftest.py
+++ hw5/code/conftest.py
@@ -2,12 +2,14 @@ import pytest
 from postgre_client.orm_client import PostgreOrmClient
 from postgre_client.client import PostgreClient
 
+PASSWORD=''
+USERNAME='postgres'
 
 @pytest.fixture(scope="session")
 def postgre_client():
-    return PostgreClient(user='postgres', password='merkyri0715', db_name='test_python', port=3452)
+    return PostgreClient(user=USERNAME, password=PASSWORD, db_name='test_python')
 
 
 @pytest.fixture(scope="session")
 def postgre_orm_client():
-    return PostgreOrmClient(user='postgres', password='merkyri0715', db_name='test_python_orm', port=3452)
+    return PostgreOrmClient(user=USERNAME, password=PASSWORD, db_name='test_python_orm')
